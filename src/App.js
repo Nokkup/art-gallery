1191cdf85fc3bfaed57e56fb341f9c51df7908b9
nokkup <kuprin.kirill.96@gmail.com>
2022-01-14 13:02:33 +0300

    feat: change interaction with the api

--- src/App.js
+++ src/App.js
@@ -1,73 +1,37 @@
 import { useState, useEffect } from "react";
-import { trackPromise } from 'react-promise-tracker';
-import API from "./api/api";
-import styles from "./styles/App.module.scss";
-import Gallery from "./components/Gallery";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Error from "./components/Error";
+import { fetchDepartments, fetchImagesListByDepartment } from "./api/api";
+import { getDefaultCategory } from "./helpers";
+import styles from "./App.module.scss";
+import Gallery from "./components/Gallery/Gallery";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  const [departments, setDepartments] = useState([]);
-  const [listIDsByCategory, setlistIDsByCategory] = useState({});
-  const [isError, setIsError] = useState(false);
+    const [departments, setDepartments] = useState([]);
+    const [listIDsByCategory, setlistIDsByCategory] = useState({});
 
-  useEffect(() => {
+    useEffect(() => {
+        async function fetchData() {
+            const departments = await fetchDepartments();
+            const defaultCategory = getDefaultCategory(departments);
+            const imageIDs = await fetchImagesListByDepartment(
+                defaultCategory.departmentId
+            );
 
-    let ids = {};
+            setDepartments(departments);
+            setlistIDsByCategory({ [defaultCategory.departmentId]: imageIDs });
+        }
 
-    trackPromise(
-    API.get(`/departments`)
-        .then(res => {
-            setDepartments(res.data.departments);
-            return res.data.departments;
-        })
-        .then(res => {
-            let defaultCategoryId = 0;
+        fetchData();
+    }, []);
 
-            res.forEach(el => {
-                if(el.displayName === "Photographs"){
-                    defaultCategoryId = el.departmentId;
-                }
-            })
-
-            API.get("/search", { params: { departmentId: defaultCategoryId, hasImages: true, q: "art"}})
-                .then(res => {
-                    ids[res.config.params.departmentId] = res.data.objectIDs;
-                    setlistIDsByCategory(ids);
-                })
-            
-            const otherRes = res.filter(el => el.departmentId !== defaultCategoryId);
-
-            const loadOtherRes = otherRes => {
-                otherRes.forEach(el => {
-                    API.get("/search", { params: { departmentId: el.departmentId, hasImages: true, q: "art"}})
-                    .then(res => {
-                        ids[res.config.params.departmentId] = res.data.objectIDs;
-                        setlistIDsByCategory(ids);
-                    })
-                })
-            }
-
-            setTimeout(loadOtherRes, 3000, otherRes);
-        }))
-        .catch(() => {
-            setIsError(true);
-        })
-
-  }, [])
-
-  return (
-    <div className={styles.wrapper}>
-      <Header />
-      {
-        isError
-          ? <Error />
-          : <Gallery idsList={listIDsByCategory} departments={departments} />
-      }
-      <Footer />
-    </div>
-  );
-}
+    return (
+        <div className={styles.wrapper}>
+            <Header />
+            <Gallery idsList={listIDsByCategory} departments={departments} />
+            <Footer />
+        </div>
+    );
+};
 
 export default App;
--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,50 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "https://collectionapi.metmuseum.org/public/collection/v1",
-    responseType: "json"
+const API_URL = "https://collectionapi.metmuseum.org/public/collection/v1";
+
+const ApiRequest = axios.create({
+    baseURL: API_URL,
+    responseType: "json",
 });
+
+export async function fetchDepartments() {
+    const data = await ApiRequest.get("/departments");
+    const departments = data.status === 200 ? data.data.departments : [];
+    return departments;
+}
+
+export async function fetchImagesListByDepartment(departmentId) {
+    const data = await ApiRequest.get("/search", {
+        params: {
+            departmentId,
+            hasImages: true,
+            q: "art",
+        },
+    });
+    const idsList = data.status === 200 ? data.data.objectIDs : [];
+    return idsList;
+}
+
+export async function fetchImageInfo(imageId) {
+    const data = await ApiRequest.get(`/objects/${imageId}`);
+    if (data.status !== 200) {
+        return {};
+    }
+    const information = {
+        imageSmallSize: data.data.primaryImageSmall,
+        imageFullSize: data.data.primaryImage,
+        author: data.data.artistDisplayName || "unknown",
+        title: data.data.title || "unknown",
+        wiki: data.data.artistWikidata_URL,
+        tags: data.data.tags?.map((el) => el.term) || [],
+    };
+
+    if (data.data.objectBeginDate !== data.data.objectEndDate) {
+        information.years = `${data.data.objectBeginDate} — ${data.data.objectEndDate}`;
+    } else {
+        information.years = data.data.objectBeginDate;
+    }
+    information.years = information.years || "undated";
+
+    return information;
+}
